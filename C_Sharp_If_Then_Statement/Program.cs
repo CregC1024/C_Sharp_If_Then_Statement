e3d01539094bc50eff72aed89b4b04695b816917
Creg Sullivan <cregcsullivan1024@gmail.com>
2022-01-11 13:38:35 -0600

    Add project files.

---
+++ C_Sharp_If_Then_Statement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+public class Program
+{
+	public static void Main()
+	{
+		int i = 10, j = 20;
+
+		if (i > j)
+		{
+			Console.WriteLine("i is greater than j");
+		}
+
+		if (i < j)
+		{
+			Console.WriteLine("i is less than j");
+		}
+
+		if (i == j)
+		{
+			Console.WriteLine("i is equal to j");
+		}
+	}
+
+    private static object GetDebuggerDisplay()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+ 
